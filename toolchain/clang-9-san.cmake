eed22b82b4369f0c0f0e5f1ac5bb713846ad7889
Felix Gündling <felix.guendling@gmail.com>
2020-04-18 10:12:30 +0200

    no ccache files

---
+++ toolchain/clang-9-san.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_C_COMPILER /usr/bin/clang-9)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++-9)
+
+set(CMAKE_C_FLAGS "-fsanitize=address,undefined -fno-omit-frame-pointer -fcolor-diagnostics" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-fsanitize=address,undefined -fno-omit-frame-pointer -fcolor-diagnostics -stdlib=libc++" CACHE STRING "" FORCE)
+
+set(BOOST_ROOT /opt/boost_1_72_0-libc++)
---
+++ toolchain/gcc-9.cmake
@@ -0,0 +1,7 @@
+set(CMAKE_C_COMPILER /usr/bin/gcc-9)
+set(CMAKE_CXX_COMPILER /usr/bin/g++-9)
+
+set(CMAKE_C_FLAGS "-fdiagnostics-color=always" CACHE STRING "" FORCE)
+set(CMAKE_CXX_FLAGS "-fdiagnostics-color=always" CACHE STRING "" FORCE)
+
+set(BOOST_ROOT /opt/boost_1_72_0)
